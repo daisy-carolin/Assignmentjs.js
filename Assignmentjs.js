0ddc69b314935fe1bb7b579af8cac60de5cb23f5
student <daisy-carolin@gmail.com>
2021-03-29 07:50:14 +0300

    first commit

---
+++ Assignmentjs.js
@@ -0,0 +1,15 @@
+var kiosk={
+    fruit:["grapes"],
+    fruit2:["mango"],
+    fruit3:["pawpaw"],
+    fruit4:["lemon"],
+    fruit5:["guava"],
+    fruit6:["orange"],
+    fruit7:["citrus"],
+    set fruit(fruit){
+},
+get_fruit(){
+
+},
+}
+console.log(kiosk.fruit7);
